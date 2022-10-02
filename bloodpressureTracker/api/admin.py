b7d7b600e62e7e5d4ec5d8e7c451d292f0afe492
Susan <susan23codes@gmail.com>
2022-10-02 14:27:37 -0400

    made model

--- bloodpressureTracker/api/admin.py
+++ bloodpressureTracker/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Reading, User
+
+admin.site.register(Reading)
+admin.site.register(User)
+
 
 # Register your models here.
--- bloodpressureTracker/api/models.py
+++ bloodpressureTracker/api/models.py
@@ -9,3 +9,10 @@ class User(AbstractUser):
     def __repr__(self):
         return f'<User username={self.username} pk={self.pk}>'
 # Create your models here.
+
+
+class Reading(models.Model):
+    systolic = models.IntegerField(blank=False, null=False)
+    diastolic = models.IntegerField(blank=False, null=False)
+    reading_time = models.DateTimeField(blank=False, null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='readings')
