6910100ea3ffa5a5b43f2d9f374c8d014edeefcf
Susan <susan23codes@gmail.com>
2022-10-16 20:09:54 -0400

    added static stuff

--- bloodpressureTracker/settings.py
+++ bloodpressureTracker/settings.py
@@ -64,7 +64,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -155,9 +155,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-# if not DEBUG:
-#     STATIC_ROOT = [BASE_DIR / "staticfiles"]
-#     STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+if not DEBUG:
+    STATIC_ROOT = [BASE_DIR / "staticfiles"]
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
